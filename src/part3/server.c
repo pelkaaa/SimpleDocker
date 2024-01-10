b97d0e57d0bcc8b0809ede7603cbb6356a19d1b7
Severobata <pelageyagerasimova@MacBook-Air-Pelageya.local>
2024-01-11 00:32:59 +0300

    1

---
+++ src/part3/server.c
@@ -0,0 +1,11 @@
+#include "fcgi_stdio.h"
+#include <stdio.h>
+
+int main() {
+    while (FCGI_Accept() >= 0) {
+        // Обработка запроса
+        printf("Content-type: text/html\r\n\r\n");
+        printf("Hello World!");
+    }
+    return 0;
+}
